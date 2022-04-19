1bf6878564d05eb44a3ee268976e56efcbd8add2
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2022-04-18 21:20:26 -0700

    object constructor version

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,31 +1,51 @@
-let students=[
-    {
-        //attr:value,
-        firstName:"Astrid",
-        lastName:"Batres-Guerrero",
-        age:25,
-        isActive:true
-    },
-    {
-        firstName:"Anthony",
-        lastName:"Lane",
-        age:22,
-        isActive:true
-    },
-    {
-        firstName:"Ben",
-        lastName:"Vance",
-        age:25,
-        isActive:false,
-        hobbies:["Listen Music","Read","Play Zelda"],
-        address:{
-            street:"Palm street",
-            zip:"22769",
-            number:"262-k"
-        }
-    }];
+// let students=[
+//     {
+//         //attr:value,
+//         firstName:"Astrid",
+//         lastName:"Batres-Guerrero",
+//         age:25,
+//         isActive:true
+//     },
+//     {
+//         firstName:"Anthony",
+//         lastName:"Lane",
+//         age:22,
+//         isActive:true
+//     },
+//     {
+//         firstName:"Ben",
+//         lastName:"Vance",
+//         age:25,
+//         isActive:false,
+//         hobbies:["Listen Music","Read","Play Zelda"],
+//         address:{
+//             street:"Palm street",
+//             zip:"22769",
+//             number:"262-k"
+//         }
+//     }];
 
-console.log(students[0].firstName);
+//object constructor(function)
+
+//these are local vars <--------------------->
+    function Students(fname,lname,age,isActive){
+        this.firstName=fname;
+        this.lastName=lname;
+        this.age=age;
+        this.isActive=isActive;
+    }
+
+let student1= new Students("Astrid","Batres-Guerrero",25,true);
+let student2=new Students("Alexis","Aldrete",25,true);
+
+console.log(student1,student2);
+
+//Exercise: 
+//Create an object contructor function and two objects
+
+
+
+
 
 function displayStudent(){
     //travel the array
@@ -39,5 +59,5 @@ function displayStudent(){
     div.innerHTML=tmp;
 }
 
-displayStudent();
+//displayStudent();
 
--- scripts/register.js
+++ scripts/register.js
@@ -9,24 +9,38 @@ let petSalon = {
         open:"9:00 a.m.",
         close:"8:00 p.m."
     },
-    pets:[
-        {
-            //obj1
-        },
-        {
-            //obj2
-        },
-        {
-            //obj3
-        }
-    ]
+    pets:[]
+}
+//name,age,gender,breed,service,owner name, contact phone
+function Pet(name,age,gender,breed,service,ownerName,contactPhone){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
+    this.owner=ownerName;
+    this.phone=contactPhone;
+}
+//get the info from the inputs and store the information
+let inputName=document.getElementById("txtName");
+let inputAge=document.getElementById("txtAge");
+let inputGender=document.getElementById("txtGender");
+
+function register(){
+    //create the pet
+    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value);
+    console.log(thePet);
+    //push the pet into the array
+    petSalon.pets.push(thePet);
+    //clear the inputs
+    clearInputs();
 }
 
-console.log(petSalon);
-function displaySalonInfo(){
+function clearInputs(){
+    inputName.value="";
 
 }
-
-function displayPetsNames(){
-    
-}
+//Create three pets
+let scooby = new Pet("Scooby",50,"Male","Dane","Grooming","Shaggy","555-555-555");
+let scrappy = new Pet("Scrappy",40,"Male","Mixed","Nails cut"," Shaggy","555-555-555");
+petSalon.push(scooby,scrappy);
