d80937e29b345b0e69c7faac8a6cc5da7be4b645
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2022-04-20 19:52:32 -0700

    displaying pets

--- scripts/register.js
+++ scripts/register.js
@@ -25,22 +25,51 @@ function Pet(name,age,gender,breed,service,ownerName,contactPhone){
 let inputName=document.getElementById("txtName");
 let inputAge=document.getElementById("txtAge");
 let inputGender=document.getElementById("txtGender");
+let inputBreed=document.getElementById("txtBreed");
+let inputService=document.getElementById("selService");
+let inputOwner = document.getElementById("txtOwner");
+let inputPhone=document.getElementById("txtTel");
+
+function isValid(aPet){
+    //return false when the pet is not valid
+    //return true if the pet is valid
+    let valid=true;
+    if(aPet.name.length==0){
+        //if we get here it means that the name is not valid
+        valid=false;
+        console.error("Invalid name");
+    }
+    if(aPet.service.length==0){
+        valid=false;
+        console.error("Invalid service");
+    }
+    if(aPet.phone.length==0){
+        valid=false;
+        console.error("Invalid phone");
+    }
+    return valid;// it could be true or false
+}
 
 function register(){
     //create the pet
-    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value);
-    console.log(thePet);
-    //push the pet into the array
-    petSalon.pets.push(thePet);
-    //clear the inputs
-    clearInputs();
+    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputPhone.value);
+    if(isValid(thePet)){
+        //push the pet into the array
+        petSalon.pets.push(thePet);
+        displayCards()
+        //clear the inputs
+        clearInputs();
+    }
+    
+    
 }
 
 function clearInputs(){
     inputName.value="";
-
+    inputAge.value="";
 }
 //Create three pets
 let scooby = new Pet("Scooby",50,"Male","Dane","Grooming","Shaggy","555-555-555");
 let scrappy = new Pet("Scrappy",40,"Male","Mixed","Nails cut"," Shaggy","555-555-555");
-petSalon.push(scooby,scrappy);
+petSalon.pets.push(scooby,scrappy);
+displayCards()
